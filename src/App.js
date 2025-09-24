d89b711087bdaf3173fa1750fbe45b2393398724
SrieeVathsan <srieevathsanpr@gmail.com>
2025-09-24 11:43:13 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -62,9 +62,9 @@ const App = () => {
   // Build FB SDK loader and login status checks
   useEffect(() => {
     if (window.FB) {
-      setFbReady(true);
       window.FB.getLoginStatus((resp) => {
         setFbStatus(resp?.status ?? "unknown");
+        setFbReady(true);
       });
       return;
     }
